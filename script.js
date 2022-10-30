69b545fe5c399e62fe350c77b1c4f86606fad87c
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2022-10-30 15:15:30 -0300

    finalizado sin persistencia

--- script.js
+++ script.js
@@ -1,44 +1,30 @@
+import checkComplete from './components/checkComplete.js';
+import deleteIcon from './components/deleteIcon.js';
 
-(()=>{  //inmediately invoked function expression IIFE
-const btn = document.querySelector("[data-form-btn]")
+const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento)=>{
-    evento.preventDefault()
-    const input = document.querySelector("[data-form-input]")
-    const value=input.value;
-    const list = document.querySelector("[data-list]")
-    const task = document.createElement(`li`);
-    task.classList.add("card") //agregando una clase
-    input.value = "";
-    //backticks
-    const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(titleTask);
-    const content = `
-    
-  
-  <i class="fas fa-trash-alt trashIcon icon"></i>`
-    //task.innerHTML = content ;
-    task.appendChild(taskContent);
-    list.appendChild(task)
-   
+const createTask = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  //backticks
+  const taskContent = document.createElement('div');
+
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+  // task.innerHTML = content;
+
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
+  list.appendChild(task);
 };
 
-btn.addEventListener("click" ,createTask)
-const checkComplete = ()=>{
-    const i = document.createElement("i")
-    i.classList.add("far" , "fa-check-square" , "icon");
-    i.addEventListener("click" , completeTask )
-    return i
-}
-
-const completeTask = ()=> {
- const element = event.target;
- element.classList.toggle("fas");//TOOGLE elimina o quita una clase
- element.classList.toggle("completeIcon")//si esta la quita y si no esta la agrega
- element.classList.toggle("far");
-}
-})();
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
